88a63ba2931f7d360f28c5cc59b20b78c35f33e7
antdp425 <antdp425@aol.com>
2020-07-04 10:33:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+let mapToNegativize = (array) => {
+   return array.map(el => {return el * -1})
+}
+
+let mapToNoChange = (array) => {
+   return array.map(el => {return el})
+}
+
+let mapToDouble = (array) => {
+   return array.map(el => {return el * 2})
+}
+
+let mapToSquare = (array) => {
+   return array.map(el => {return el ** 2})
+}
+
+let reduceToTotal = (array,start = 0) => {
+   return array.reduce((el,result) =>{
+      return result += el
+   }, start)
+}
+
+let reduceToAllTrue = (array) => {
+   return array.every(el => {return !!el})
+}
+
+let reduceToAnyTrue = (array) => {
+   return array.some(el => {return !!el})
+
+}
