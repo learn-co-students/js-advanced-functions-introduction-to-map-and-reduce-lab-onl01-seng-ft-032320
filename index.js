3b4a8d9482ddfbe7781ce0986b89e7a7405314a8
Nashmeyah <nashalrekabi@gmail.com>
2020-07-08 12:59:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+let mapToNegativize = (array) => {
+   return array.map(n => {return n * -1})
+}
+
+let mapToNoChange = (array) => {
+   return array.map(n => {return n})
+}
+
+let mapToDouble = (array) => {
+   return array.map(n => {return n * 2})
+}
+
+let mapToSquare = (array) => {
+   return array.map(n => {return n ** 2})
+}
+
+let reduceToTotal = (array,start = 0) => {
+   return array.reduce((n,result) => {return result += n}, start)
+}
+
+let reduceToAllTrue = (array) => {
+   return array.every(n => {return !!n})
+}
+
+let reduceToAnyTrue = (array) => {
+   return array.some(n => {return !!n})
+}
