d17d2657fffcd7715cdad5543cfbcb5c9f8ac334
nisticism <nistacular@gmail.com>
2020-07-15 22:39:55 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Your code here
+
+function mapToNegativize(array) {
+    return array.map(element => element * -1);
+}
+
+function mapToNoChange(array) {
+    return array;
+}
+
+function mapToDouble(array) {
+    return array.map(element => element * 2);
+}
+
+function mapToSquare(array) {
+    return array.map(element => element * element);
+}
+
+function reduceToTotal(array, startingPoint) {
+    return array.reduce(function(total, element) {
+        if (startingPoint) {
+            total += startingPoint/2;
+        }
+        return element + total});
+}
+
+function reduceToAllTrue(array) {
+    return array.reduce(function(total, element) {
+        for (let i = 0; i < array.length; i ++) {
+            if (element == false) {
+                return false;
+            }
+        }
+        return true;
+    });
+}
+
+function reduceToAnyTrue(array) {
+    return array.reduce(function(total, element) {
+        for (let i = 0; i < array.length; i ++) {
+            if (element == true) {
+                return true;
+            }
+
+        }
+        return false;
+    });
+}
+
+
